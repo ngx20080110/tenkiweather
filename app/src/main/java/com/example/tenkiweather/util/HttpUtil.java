7756ce4af92a5100c3fd36be7577240015df1a41
Guan Ying WU <gy.wu@macaowater.com>
2018-02-09 17:16:58 +0800

    add utility class

---
+++ app/src/main/java/com/example/tenkiweather/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.example.tenkiweather.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by wugy on 9/2/2018.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
